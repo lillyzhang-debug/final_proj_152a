323bbb7aac84cf82a7a26ff7277f07b2e1f15b5a
erinzhu234 <117385419+brodyjones@users.noreply.github.com>
2025-11-24 11:47:35 -0800

    added more util to controller, including start_up functionality

--- final_project.srcs/sources_1/new/menu.v
+++ final_project.srcs/sources_1/new/menu.v
@@ -14,8 +14,9 @@ module controller (
     output reg [3:0] thousands,
     output reg [3:0] LED_on
     );
-reg [4:0] duration;
-reg started;
+reg [4:0] duration = 0;
+reg started = 0; // have we started the game
+reg [1:0] start_up = 3; // do startup actions before actually starting
 reg [14:0] count;
 integer i = 0;
     // always on the ms, update display based on if in menu = 0, game = 1
@@ -27,6 +28,10 @@ integer i = 0;
             hundreds <= (score/100) % 10;
             thousands <= score/1000;
             started <= 0;
+            start_up <= 3; // ?
+            if (duration != 0) begin
+                //flash final score here
+            end
         end
         if(mode == 1) begin
             // check sw to determine game mode
@@ -47,28 +52,40 @@ integer i = 0;
                 else begin
                     // final score should already be set
                     duration <= 3; // want to flash for 3 seconds
+                    // directly flash the final score here???
                     started <= 0; // have not started the game
                 end
             end
             if (started == 1) begin
-                count <= count + 1;
-                // if we've reached .8 ms increment, flash a random LED and turn all others off no matter what
-                if (count % 800 == 0) begin
-                    // check which rand LED is to be flashed
-                    // set it to on until the user presses or time runs out
-                    for(i = 0; i < 4; i = i + 1) begin
-                        if(i == LED_2_disp)
-                            LED_on[LED_2_disp] <= 1;
-                        else
-                            LED_on[i] <= 0;
-                     end
+                if (start_up) begin
+                    // flash duration on display, 3 times per second
+                    // ideally this blinks? probably do that somewhere else
+                    ones <= 0;
+                    tens <= duration/10;
+                    hundreds <= 0;
+                    thousands <= 0;
+                    start_up <= start_up - 1; // set back to 3 at game end
                 end
-                // check if the user has pressed the right button
-                // if so turn the flashed LED off
-                for (i = 0; i < 4; i = i + 1) begin
-                    if(LED_2_disp == keypad)
-                        LED_on[LED_2_disp] <= 0;
-                end
+                else begin
+                    count <= count + 1;
+                    // if we've reached .8 ms increment, flash a random LED and turn all others off no matter what
+                    if (count % 800 == 0) begin
+                        // check which rand LED is to be flashed
+                        // set it to on until the user presses or time runs out
+                        for(i = 0; i < 4; i = i + 1) begin
+                            if(i == LED_2_disp)
+                                LED_on[LED_2_disp] <= 1;
+                            else
+                                LED_on[i] <= 0;
+                         end
+                    end
+                    // check if the user has pressed the right button
+                    // if so turn the flashed LED off
+                    for (i = 0; i < 4; i = i + 1) begin
+                        if(LED_2_disp == keypad)
+                            LED_on[LED_2_disp] <= 0;
+                    end
+                 end
             end    
                                 
         end 
